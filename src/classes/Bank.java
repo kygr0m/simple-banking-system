aadf054ad7b374f95ad40bcd5c4ad86bacd5d4b6
Your Name <dalcal@protonmail.com>
2024-10-01 20:32:35 +0100

    BankAccount methods added

--- src/classes/Bank.java
+++ src/classes/Bank.java
@@ -5,8 +5,8 @@ public class Bank
 
 	public static void main(String[]args)
 	{
-		 BankAccount acc1 = new BankAccount(20);
-		 BankAccount acc2 = new BankAccount(20);
+		 BankAccount acc1 = new BankAccount(20,30,40);
+		 BankAccount acc2 = new BankAccount(20,50,54);
 		 Client client1 = new Client("Jakob", acc1);
 		 Client client2 = new Client("Marie", acc2);
 		 
--- src/classes/BankAccount.java
+++ src/classes/BankAccount.java
@@ -1,17 +1,73 @@
 package classes;
+import java.util.Scanner;
 
 public class BankAccount
 {
-	private int number;
+	private int countryCode;
+	private int twoCheckDigit;
+	private int bban;
+	private String iban;
 	
-	public BankAccount(int number)
+	
+	public  BankAccount()
 	{
-		setNumber(number);
+		
 	}
 	
-	private void setNumber(int number)
+
+	public  BankAccount(String iban)
 	{
-		this.number = number;
+		this.iban = iban;
 	}
 	
+	public BankAccount(int countryCode , int twoCheckDigit, int bban )
+	{
+		this.countryCode = countryCode;
+		this.twoCheckDigit = twoCheckDigit;
+		this.bban = bban;
+		
+		this.iban =	setIban(countryCode, twoCheckDigit , bban );
+	}
+	
+	
+	
+	
+	public BankAccount createBankAccount()
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Add the country code");
+		int countryCode = scan.nextInt();
+		
+		System.out.println("Add the two check digits");
+		int twoCheckDigit = scan.nextInt();
+		
+		System.out.println("Add the bban");
+		int bban = scan.nextInt();
+		
+		
+		BankAccount bankacc = new BankAccount(countryCode, twoCheckDigit, bban );
+		
+		return bankacc;
+
+
+	}
+	
+	
+	private String setIban(int countryCode , int twoCheckDigit, int bban)
+	{
+	
+		return Integer.toString(countryCode) + Integer.toString(twoCheckDigit) + Integer.toString(bban);
+	}
+	
+	
+	/*private int randomNumber()
+	{
+		int max = 
+	}*/
+	
+	public String toString()
+	{
+		return iban; 
+	}
+
 }
